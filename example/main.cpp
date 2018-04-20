c0f72234d823a854fd6f848e02722f832cdc5f8e
Chris Jones <cmsj@tenshu.net>
2018-04-20 21:13:35 +0100

    Tweak layout

--- example/main.cpp
+++ example/main.cpp
@@ -112,7 +112,7 @@ static int selected_app = 0;
                 ImGui::Dummy(ImVec2(0.0f, 5.0f));
 
                 ImGui::PushItemWidth(-1.0f);
-                ImGui::ListBox("", &selected_app, apps, IM_ARRAYSIZE(apps), 10);
+                ImGui::ListBox("", &selected_app, apps, IM_ARRAYSIZE(apps), 11);
                 break;
             case BT_WIN_UPDATE:
                 break;
