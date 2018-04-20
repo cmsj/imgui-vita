382a596476c7a8d306c108defc5fc8602499595e
Chris Jones <cmsj@tenshu.net>
2018-04-20 20:29:21 +0100

    Tweak layout

--- example/main.cpp
+++ example/main.cpp
@@ -73,12 +73,12 @@ static int current_category = 0;
         ImGui::Begin("Main Window", NULL, fullscreenFlags);
 
         // This is effectively a poor man's tab bar, but ImGui doesn't have native tabs yet
+        ImGui::SameLine();
         if (ImGui::Button("Browse")) current_window = BT_WIN_BROWSE;
-        ImGui::SameLine();
         if (ImGui::Button("Search")) current_window = BT_WIN_SEARCH;
-        ImGui::SameLine();
         if (ImGui::Button("Updates")) current_window = BT_WIN_UPDATE;
-        ImGui::SameLine();
+        ImGui::NewLine();
+        ImGui::Separator();
 
         // BEGIN: Contents
         switch (current_window) {
