51b49de7dd61c89bf23fce207ed97244ff93e4c4
Chris Jones <cmsj@tenshu.net>
2018-04-20 12:01:36 +0100

    Start vita-ifying the main window

--- example/main.cpp
+++ example/main.cpp
@@ -23,7 +23,8 @@ int main(int, char**)
 	// Setup style
 	ImGui::StyleColorsDark();
 
-	bool show_demo_window = true;
+    bool show_main_window = true;
+	bool show_demo_window = false;
 	bool show_another_window = false;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
@@ -34,41 +35,19 @@ int main(int, char**)
 		vglStartRendering();
 		ImGui_ImplVitaGL_NewFrame();
 		
-		if (ImGui::BeginMainMenuBar()){
-			if (ImGui::BeginMenu("Debug")){
-				ImGui::EndMenu();
-			}
-			ImGui::EndMainMenuBar();
-		}
-		
 		// 1. Show a simple window.
 		// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
 		{
-			static float f = 0.0f;
-			static int counter = 0;
-			ImGui::Text("Hello, world!");						   // Display some text (you can use a format string too)
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);			// Edit 1 float using a slider from 0.0f to 1.0f	
-			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-			ImGui::Checkbox("Demo Window", &show_demo_window);	  // Edit bools storing our windows open/close state
-			ImGui::Checkbox("Another Window", &show_another_window);
-
-			if (ImGui::Button("Button"))							// Buttons return true when clicked (NB: most widgets return true when edited/activated)
-				counter++;
-			ImGui::SameLine();
-			ImGui::Text("counter = %d", counter);
+            IMGui::SetNextWindowPos(ImVec2(0, 0));
+            IMGui::SetNextWindowSize(ImVec2(io.DisplaySize.x, io.DisplaySize.y)); 
+            IMGui::Begin("Main Window", &show_main_window, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar);
+            IMGui::Text("Welcome to Dear imgui for Vita.");
+            if (IMGui::Button("Show demo window")) {
+                show_demo_window = true;
+            }
+            IMGui::End();
 
 			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		}
-
-		// 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
-		if (show_another_window)
-		{
-			ImGui::Begin("Another Window", &show_another_window);
-			ImGui::Text("Hello from another window!");
-			if (ImGui::Button("Close Me"))
-				show_another_window = false;
-			ImGui::End();
 		}
 
 		// 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
